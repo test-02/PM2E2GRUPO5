50c86d2df17abc0f8cf4657461193c5e559f6114
Test02 <t36t02@gmail.com>
2023-03-19 09:01:52 -0600

    Agregar archivos de proyecto.

---
+++ PM2E2GRUPO5/Controllers/SitioController.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PM2E2GRUPO5.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM2E2GRUPO5.Controllers
+{
+    public class SitioController
+    {
+    private static readonly string URL_SITIOS = "http://192.168.1.10/examenmovilgrupo5/";
+    private static HttpClient client = new HttpClient();
+
+        /*public static async Task<List<SitiosFirma>> GetAllSite()
+        {
+            List<SitiosFirma> listBooks = new List<SitiosFirma>();
+            try
+            {
+                var uri = new Uri(URL_SITIOS + "GetSitios.php");
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    listBooks = JsonConvert.DeserializeObject<List<SitiosFirma>>(content);
+                    return listBooks;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error 1: " + ex.Message);
+            }
+
+            return listBooks;
+        }*/
+
+        public static async Task<List<SitiosFirma>> GetAllSite()
+        {
+            List<SitiosFirma> listBooks = new List<SitiosFirma>();
+            try
+            {
+                var uri = new Uri(URL_SITIOS + "GetSitios.php");
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var jobject = JObject.Parse(content);
+                    var sitioArray = JArray.Parse(jobject["sitio"].ToString());
+                    foreach (var sitioObject in sitioArray)
+                    {
+                        var sitio = new SitiosFirma();
+                        sitio.Id = Convert.ToInt32(sitioObject["id"]);
+                        sitio.Descripcion = sitioObject["descripcion"].ToString();
+                        sitio.Latitud = sitioObject["latitud"].ToString();
+                        sitio.Longitud = sitioObject["longitud"].ToString();
+                        listBooks.Add(sitio);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetSitios: " + ex.Message);
+            }
+            return listBooks;
+        }
+    public async static Task<bool> DeleteSite(string id)
+    {
+        try
+        {
+            var uri = new Uri(URL_SITIOS + "eliminarsitio.php?id=" + id);
+            var result = await client.GetAsync(uri);
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return false;
+    }
+
+    public async static Task<bool> CreateSite(SitiosFirma sitio)
+    {
+        try
+        {
+            Uri requestUri = new Uri(URL_SITIOS + "Create.php");
+            var jsonObject = JsonConvert.SerializeObject(sitio);
+            var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(requestUri, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Create" + ex.Message);
+        }
+
+        return false;
+    }
+
+
+ public async static Task<bool> UpdateSitio(SitiosFirma sitio)
+    {
+        try
+        {
+            Uri requestUri = new Uri(URL_SITIOS + "actualizarsitio.php");
+            var jsonObject = JsonConvert.SerializeObject(sitio);
+            var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+            //var response = await client.PutAsync(requestUri, content);
+            var response = await client.PostAsync(requestUri, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return false;
+    }
+
+}
+}
---
+++ PM2E2GRUPO5/MainPage.xaml.cs
@@ -0,0 +1,260 @@
+﻿using Nancy.Json;
+using Plugin.AudioRecorder;
+using PM2E2GRUPO5.Models;
+using PM2E2GRUPO5.Controllers;
+using SignaturePad.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PM2E2GRUPO5
+{
+    public partial class MainPage : ContentPage
+    {
+
+        bool flag2 = false;
+
+        private readonly AudioRecorderService audioRecorderService = new AudioRecorderService();
+
+        SitioController sitiosApi;
+        List<SitiosFirma> ListaSitios;
+        public MainPage()
+        {
+            InitializeComponent();
+            checkInternet();
+            getLocation();
+
+            sitiosApi = new SitioController();
+            ListaSitios = new List<SitiosFirma>();
+            flag2 = false;
+
+        }
+
+        private async void btnUbicaciones_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.PageListaUbicaciones());
+
+        }
+
+        private void Limpiardescripcion_Clicked(object sender, EventArgs e)
+        {
+            descripcion.Text = null;
+
+        }
+
+        private async void btngrabarvoz_Clicked(object sender, EventArgs e)
+        {
+
+            var status = await Permissions.RequestAsync<Permissions.Microphone>();
+            var status2 = await Permissions.RequestAsync<Permissions.StorageRead>();
+            var status3 = await Permissions.RequestAsync<Permissions.StorageWrite>();
+            if (status != PermissionStatus.Granted && status2 != PermissionStatus.Granted && status3 != PermissionStatus.Granted)
+            {
+                return; // si no tiene los permisos no avanza
+            }
+
+            onda1.IsVisible = true;
+            onda2.IsVisible = true;
+            ondaespacio.IsVisible = false;
+            imgmicro.Source = "voice.png";
+            btnGuardar.IsEnabled = false;
+            btngrabarvoz.IsVisible = false;
+            btndetenervoz.IsVisible = true;
+
+            await audioRecorderService.StartRecording();
+
+            flag2 = true;
+
+        }
+
+        private async void btndetenervoz_Clicked(object sender, EventArgs e)
+        {
+
+            onda1.IsVisible = false;
+            onda2.IsVisible = false;
+            ondaespacio.IsVisible = true;
+            ondaespacio.Text = "¡Guardado!";
+            imgmicro.Source = "voiceoff.png";
+            btnGuardar.IsEnabled = true;
+            btngrabarvoz.IsVisible = true;
+            btndetenervoz.IsVisible = false;
+
+            await audioRecorderService.StopRecording();
+
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+            bool flag1 = false;
+            if (latitud.Text == null || longitud.Text == null)
+            {
+                flag1 = true;
+                await DisplayAlert("Error", "Se necesitan las coordenadas de su ubicación para guardar.", "OK");
+            }
+
+            if (descripcion.Text == null || descripcion.Text == "")
+            {
+                flag1 = true;
+                await DisplayAlert("Error", "Se necesita una breve descripción de la ubicación.", "OK");
+            }
+
+            if (!flag1)
+            {
+                byte[] ImageBytes = null;
+                byte[] AudioBytes = null;
+                var firma = PadView.Strokes;
+
+
+                //obtenemos la firma
+                try
+                {
+                    var image = await PadView.GetImageStreamAsync(SignatureImageFormat.Png);
+
+                    //Pasamos la firma a imagen a base 64
+                    var mStream = (MemoryStream)image;
+                    byte[] data = mStream.ToArray();
+                    string base64Val = Convert.ToBase64String(data);
+                    ImageBytes = Convert.FromBase64String(base64Val);
+                }
+                catch (Exception error)
+                {
+                    await DisplayAlert("Aviso", "No has ingresado tu firma", "OK");
+                    return;
+                }
+
+                //obtenemos el audio
+                try
+                {
+                    var audio = audioRecorderService.GetAudioFileStream();
+
+
+                    AudioBytes = File.ReadAllBytes(audioRecorderService.GetAudioFilePath());
+                }
+                catch (Exception error)
+                {
+                    if (flag2)
+                    {
+                        await DisplayAlert("Aviso", "No has hablado fuerte al grabar tu nota de voz", "OK");
+                    }
+                    else
+                    {
+                        await DisplayAlert("Aviso", "No has grabado una nota de voz", "OK");
+                    }
+
+                    return;
+                }
+
+                try
+                {
+                    byte[] a;
+                    var serializer = new JavaScriptSerializer();
+
+                    var b = serializer.Serialize(firma);
+
+                    a = null;
+
+                    SitiosFirma sitio = new SitiosFirma
+                    {
+                        Descripcion = descripcion.Text,
+                        Latitud = latitud.Text,
+                        Longitud = longitud.Text,
+                        FirmaDigital = ImageBytes,
+                        AudioFile = AudioBytes,
+                        firma = b
+                    };
+
+                    await SitioController.CreateSite(sitio);
+                    await DisplayAlert("Aviso", "Sitio ingresado con éxito: " + sitio.Descripcion, "OK");
+                    PadView.Clear();
+                    descripcion.Text = null;
+
+                }
+                catch (Exception error)
+                {
+                    await DisplayAlert("Aviso", "" + error, "OK");
+                }
+
+
+            }
+
+        }
+
+        #region Location
+        private void cleanLocation()
+        {
+            latitud.Text = null;
+            longitud.Text = null;
+        }
+
+        public async void getLocation()
+        {
+            try
+            {
+                var location = await Geolocation.GetLocationAsync();
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    latitud.Text = "" + location.Latitude;
+                    longitud.Text = "" + location.Longitude;
+                    //await DisplayAlert("Aviso", "Si se leyó la ubicacion: "+location.Latitude +", "+location.Longitude, "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Aviso", "El GPS está desactivado o no puede reconocerse", "OK");
+                    cleanLocation();
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                await DisplayAlert("Aviso", "Este dispositivo no soporta la versión de GPS utilizada", "OK");
+                cleanLocation();
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                //await DisplayAlert("Aviso", "Handle not enabled on device exception: "+fneEx, "OK");
+                await DisplayAlert("Aviso", "La ubicación está desactivada", "OK");
+                cleanLocation();
+
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                await DisplayAlert("Aviso", "La aplicación no puede acceder a su ubicación.\n\n" +
+                    "Habilite los permisos de ubicación en los ajustes del dispositivo", "OK");
+                cleanLocation();
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                await DisplayAlert("Aviso", "No se ha podido obtener la localización (error de gps)", "OK");
+                cleanLocation();
+            }
+        }
+        #endregion
+
+        #region Internet
+        public async void checkInternet()
+        {
+            //await DisplayAlert("Aviso", "si", "OK");
+            var current = Connectivity.NetworkAccess;
+
+            if (current != NetworkAccess.Internet)
+            {
+                // Connection to internet is available
+                await DisplayAlert("Aviso", "Usted no tiene acceso a Internet.\n" +
+                    "El acceso a Internet es requerido para el buen funcionamiento de la aplicación.", "OK");
+            }
+
+        }
+        #endregion
+
+
+    }
+}
